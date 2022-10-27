31480804ceb3e170e1f46edcd32d3e0f02b50607
Maks-Budiukin <106489459+Maks-Budiukin@users.noreply.github.com>
2022-10-28 01:51:49 +0300

    done 2 part

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,15 +3,15 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryEl = document.querySelector('.gallery');
 
-const imageEl = galleryItems.map((image) => 
+const imageEl = galleryItems.map(({preview, original, description}) => 
     
     `<div class="gallery__item">
-  <a class="gallery__link" href="${image.original}">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
-      src="${image.preview}"
-      data-source="${image.original}"
-      alt="${image.description}"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
 </div>`).join("");
@@ -24,19 +24,15 @@ function showBigImage(evt) {
     evt.preventDefault();
     
     if (evt.target.nodeName === 'IMG') {
-        const instance = basicLightbox.create(`
-    <img src="${evt.target.dataset.source}" width="800" height="600">
-`);
+        const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}" width="800" height="600">`);
+        
         instance.show(() => window.addEventListener('keydown', onEscPress));
 
         function onEscPress(evt) {
             if (evt.code === 'Escape') {
         instance.close(() => window.removeEventListener('keydown', onEscPress));
+            }
+        }
     }
-}
-        
-    }
-
-    
 }
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const imageEl = galleryItems.map(({preview, original, description}) => 
+    
+    `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`).join("");
+
+galleryEl.insertAdjacentHTML('beforeend', imageEl);
+
+galleryEl.addEventListener('click', showBigImage)
+
+function showBigImage(evt) {
+    evt.preventDefault();
+    
+    if (evt.target.nodeName === 'IMG') {
+        const lightbox = new SimpleLightbox('.gallery a',{ captionsData: 'alt', captionDelay: 250});
+    }
+}
