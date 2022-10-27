de9e09cc5499a5dd9dabc15000a9b034d8966acc
Maks-Budiukin <106489459+Maks-Budiukin@users.noreply.github.com>
2022-10-27 23:44:09 +0300

    done 1 part

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const imageEl = galleryItems.map((image) => 
+    
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`).join("");
+
+galleryEl.insertAdjacentHTML('beforeend', imageEl);
+
+galleryEl.addEventListener('click', showBigImage)
+
+function showBigImage(evt) {
+    evt.preventDefault();
+    
+    if (evt.target.nodeName === 'IMG') {
+        const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+        instance.show(() => window.addEventListener('keydown', onEscPress));
+
+        function onEscPress(evt) {
+            if (evt.code === 'Escape') {
+        instance.close(() => window.removeEventListener('keydown', onEscPress));
+    }
+}
+        
+    }
+
+    
+}
+
